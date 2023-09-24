acdf2d58398f305a515f9b0c695c818bc1fa844f
Osigelialex <osigelialex@gmail.com>
2023-09-24 20:12:45 +0100

    switch to prod db

--- Defectly/settings.py
+++ Defectly/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'Defectly.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -133,6 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Render postgreSQL database
 import dj_database_url
 
-# DATABASES = {
-#     'default': dj_database_url.parse(env('DATABASE_URL')),
-# }
+DATABASES = {
+    'default': dj_database_url.parse(env('DATABASE_URL')),
+}
