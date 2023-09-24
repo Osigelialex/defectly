ec312284c0c6ceef55340a8ad83ea90cbf783e92
Osigelialex <osigelialex@gmail.com>
2023-09-24 14:10:15 +0100

    set allowed hosts

--- Defectly/Defectly/settings.py
+++ Defectly/Defectly/settings.py
@@ -28,9 +28,9 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
