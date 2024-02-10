43386be37c9c4362ef6a6f95ec08f710a874befc
Osigelialex <osigelialex@gmail.com>
2024-02-10 12:32:02 +0100

    Switched to sqlite and fixed .env error

--- Defectly/settings.py
+++ Defectly/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import environ
 
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
